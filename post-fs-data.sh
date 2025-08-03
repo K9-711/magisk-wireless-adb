74fa20543609d91994cb3aa3b17c14f18f2de587
K9-711 <87531064+K9-711@users.noreply.github.com>
2025-08-03 23:33:48 +0200

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,7 +1,11 @@
 #!/system/bin/sh
-# Enable ADB over TCP/IP on boot using post-fs-data
+
+# Log to see if script runs
+echo "Wireless ADB module started at $(date)" >> /sdcard/wireless_adb_log.txt
 
 sleep 5
 setprop service.adb.tcp.port 5555
 stop adbd
 start adbd
+
+echo "Wireless ADB module finished at $(date)" >> /sdcard/wireless_adb_log.txt
